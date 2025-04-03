77044b8bdcd10ad778424485fd2eaac3a12e275e
nyrr <57556501+nyrr@users.noreply.github.com>
2025-04-03 21:38:05 +0800

    Update f2.js

--- f2.js
+++ f2.js
@@ -1,15 +1,17 @@
 var myLinks = [
 "http://mp.musz.info",
 "http://cv.musz.info",
-"http://gfivyrc.s602.xrea.com",
-"http://redp.s1009.xrea.com",
-"http://it.alrau.com",
 "http://it.ivyrc.com",  
 "http://ce.ivyrc.com",
 "http://fappening.ivyrc.com",
 "http://br.ivyrc.com", 
+"http://it.alrau.com",
+"http://gfivyrc.s602.xrea.com",
+"http://redp.s1009.xrea.com",
 "http://tnayr.s1007.xrea.com",
 "http://lrmusz.s1007.xrea.com",
+'http://indoviral.doorblog.jp', 
+'http://myrf.doorblog.jp', 
 "http://celebfq.blogspot.com/?m=1", 
 "https://dlmusz.blogspot.com/?m=1", 
 "https://tntita.blogspot.com/?m=1", 
@@ -21,8 +23,6 @@ var myLinks = [
 "https://wissindo.blogspot.com/?m=1", 
 "https://nycmetropolitan.blogspot.com/?m=1", 
 "https://tnayrc.blogspot.com/?m=1", 
-"http://indoviral.doorblog.jp", 
-"http://myrf.doorblog.jp", 
 "https://spichen.blogspot.com/?m=1", 
 "https://2danbi.blogspot.com/?m=1", 
 "https://celebalt.blogspot.com/?m=1", 
@@ -31,7 +31,13 @@ var myLinks = [
 "https://waremp3.blogspot.com/?m=1",
 "https://celebruu.blogspot.com/?m=1",
 "https://meggiita.blogspot.com/?m=1",
-"https://kbjblogmusz.blogspot.com/?m=1"
+"https://kbjblogmusz.blogspot.com/?m=1",
+'http://uks.starfree.jp',
+'http://myrs.starfree.jp',
+'http://osak.starfree.jp',
+'http://swpr.starfree.jp',
+'http://ryc.starfree.jp',
+'http://cli.starfree.jp'
 ]
 var myLinks1 = myLinks.sort( () => (Math.random() - .5) ).slice(0, 10);
 var myLinks2 = myLinks.sort( () => (Math.random() - .5) ).slice(0, 10);
